955d04da980c9008bcb63eb1a30c5261146b75e4
IlnurKam <kamal673@yandex.ru>
2022-02-03 22:20:38 +0300

    Добавьте файлы проекта.

---
+++ Zadanie17/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie17
+{
+    class Employee<T>
+    {
+        public T Num { get; set; }
+        public string Name { get; set; }
+        public int Summa { get; set; }
+
+
+        public string GetInfo()
+        {
+            return $"{Num} {Name} {Summa}";
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер");
+            string stringNum = Console.ReadLine();
+            Num = (T)Convert.ChangeType(stringNum, typeof(T));
+
+            Console.WriteLine("Введите имя");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите зп");
+            Summa = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ Zadanie17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<int> employee1 = new Employee<int>();
+            employee1.Input();
+            Console.WriteLine(employee1.GetInfo());
+
+            Employee<string> employee2 = new Employee<string>();
+            employee2.Input();
+            Console.WriteLine(employee2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
